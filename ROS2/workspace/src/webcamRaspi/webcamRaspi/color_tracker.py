be5116705481224af13428b60a0145ce0c579173
Igor Santos <igordossantos@processoagil.com.br>
2026-01-22 16:28:12 +0000

    Refactor ROS2 packages: Add module docstrings, improve class and method documentation, and enhance code readability
    - Added module docstrings to `pubsubsimples`, `webcam`, and `webcamRaspi` packages.
    - Improved class and method docstrings in `publisher.py`, `subscriber.py`, `capturar_webcam.py`, `display_image.py`, `color_tracker.py`, `img_display.py`, `img_filter.py`, `pos_to_cmd_vel.py`, and `usb_cam.py`.
    - Renamed callback methods to follow Python naming conventions.
    - Removed unused test files related to copyright, flake8, pep257, and xmllint from both `pubsubsimples` and `webcam` packages.
    - Updated setup.py files to use uppercase package names for consistency.
    - Enhanced logging messages for better clarity during execution.

---
+++ ROS2/workspace/src/webcamRaspi/webcamRaspi/color_tracker.py
@@ -0,0 +1,51 @@
+# FAZER Procura por uma cor, dá a posição do objeto de saída
+"Docstring"
+import sys
+from rclpy.node import Node
+import rclpy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from cv_bridge import CvBridge
+# import cv2 as cv
+
+
+class ColorTracker(Node):
+    "Docstring"
+
+    def __init__(self, TopicPub="pos", TopicSub="image_filtered"):
+        super().__init__("display_camera")
+        self.get_logger().info("Display_camera iniciado!")
+        self.sub = self.create_subscription(Image, TopicSub, self.hear, 10)
+        self.pub = self.create_publisher(Point, TopicPub, 10)
+        self.bridge = CvBridge()
+
+    def hear(self):
+        "Docstring"
+        self.get_logger().info("Eu ouvi hein...")
+        # photo = self.bridge.imgmsg_to_cv2(message)
+
+        # APLICAR ALGORITMO PARA ENCONTRAR A COR
+        position = Point()
+        self.pub.publish(position)
+
+    def publish(self, position):
+        "Docstring"
+        self.get_logger().info("Vou mandar aqui olha")
+        self.pub.publish(position)
+
+
+def main():
+    "Docstring"
+    rclpy.init()
+    if len(sys.argv) == 1:
+        node = ColorTracker()
+    elif len(sys.argv) == 2:
+        node = ColorTracker(sys.argv[1])
+    else:
+        print("Usage: ros2 run <pkg> <node> <topicname>")
+        sys.exit()
+    rclpy.spin(node)
+
+
+if __name__ == "__main__":
+    main()
